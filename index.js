3988607f91234a70ace329abe06d89f9c7f07b37
ecila7290 <jihoon.kim89@gmail.com>
2021-01-27 21:42:17 +0900

    Add: added simple api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs=require('fs')
 const { clearScreenDown } = require('readline')
 const http = require('http')
+const url = require('url')
 
 //FILES
 
@@ -27,10 +28,31 @@ const http = require('http')
 
 //SERVER
 
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const dataObj=JSON.parse(data)
+
 const server=http.createServer((req,res)=>{
-    console.log(req)
-    res.end('Hello from the server')
+    const pathName=req.url
+
+    if (pathName==='/' || pathName==='/overview') {
+        res.end('this is OVERVIEW')
+    } else if (pathName==='/product') {
+        res.end('this is PRODUCT')
+    } else if (pathName==='/api') {
+        res.writeHead(200, {
+            'Content-type':'application/json'
+        })
+        res.end(data)
+        
+    } else {
+        res.writeHead(404, {
+            'Content-type':'text/html',
+        })
+        res.end('<h1>page not found</h1>')
+    }
+    
 })
+
 server.listen(8000,'172.19.160.158',()=>{
     console.log('Listening to requests on port 8000')
 })
